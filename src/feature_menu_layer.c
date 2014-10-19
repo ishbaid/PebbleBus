40fa0dd746c4fb8234f6d4c363f65b84fb3f6ce3
CloudSeekr <justin.morrow81@gmail.com>
2014-10-18 21:20:14 -0400

    asdfj

--- src/feature_menu_layer.c
+++ src/feature_menu_layer.c
@@ -1,32 +1,17 @@
 #include "pebble.h"
 
-#define NUM_MENU_SECTIONS 2
-#define NUM_MENU_ICONS 3
-#define NUM_FIRST_MENU_ITEMS 3
-#define NUM_SECOND_MENU_ITEMS 1
+#define NUM_FIRST_MENU_ITEMS 2
 
 static Window *window;
+static Window *n_campus;
+static Window *c_campus;
+
 
 // This is a menu layer
 // You have more control than with a simple menu layer
 static MenuLayer *menu_layer;
-
-// Menu items can optionally have an icon drawn with them
-static GBitmap *menu_icons[NUM_MENU_ICONS];
-
-static int current_icon = 0;
-
-// You can draw arbitrary things in a menu item such as a background
-static GBitmap *menu_background;
-
-//header text
-const Layer *header;
-
-// A callback is used to specify the amount of sections of menu items
-// With this, you can dynamically add and remove sections
-static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
-  return NUM_MENU_SECTIONS;
-}
+static MenuLayer *n_campus_layer;
+static MenuLayer *c_campus_layer;
 
 // Each section has a number of items;  we use a callback to specify this
 // You can also dynamically add and remove items using this
@@ -35,32 +20,8 @@ static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t secti
     case 0:
       return NUM_FIRST_MENU_ITEMS;
 
-    case 1:
-      return NUM_SECOND_MENU_ITEMS;
-
     default:
       return 0;
-  }
-}
-
-// A callback is used to specify the height of the section header
-static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  // This is a define provided in pebble.h that you may use for the default height
-  return MENU_CELL_BASIC_HEADER_HEIGHT;
-}
-
-// Here we draw what each header is
-static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
-  // Determine which section we're working with
-  switch (section_index) {
-    case 0:
-      // Draw title text in the section header
-      menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
-      break;
-
-    case 1:
-      menu_cell_basic_header_draw(ctx, cell_layer, "One more");
-      break;
   }
 }
 
@@ -72,33 +33,15 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
       // Use the row to specify which item we'll draw
       switch (cell_index->row) {
         case 0:
-          // This is a basic menu item with a title and subtitle
-          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
-        //define header  
-        header = cell_layer;
+          // This is a basic menu item with a title
+          menu_cell_basic_draw(ctx, cell_layer, "North Campus", NULL, NULL);
           break;
-
+        
         case 1:
-          // This is a basic menu icon with a cycling icon
-          menu_cell_basic_draw(ctx, cell_layer, "Icon Item", "Select to cycle", menu_icons[current_icon]);
-          break;
-
-        case 2:
-          // Here we use the graphics context to draw something different
-          // In this case, we show a strip of a watchface's background
-          graphics_draw_bitmap_in_rect(ctx, menu_background,
-              (GRect){ .origin = GPointZero, .size = layer_get_frame((Layer*) cell_layer).size });
+          menu_cell_basic_draw(ctx, cell_layer, "Central Campus", NULL, NULL);
           break;
       }
       break;
-
-    case 1:
-      switch (cell_index->row) {
-        case 0:
-          // There is title draw for something more simple than a basic menu item
-          menu_cell_title_draw(ctx, cell_layer, "Final Item");
-          break;
-      }
   }
 }
 
@@ -107,27 +50,17 @@ void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *da
   // Use the row to specify which item will receive the select action
   switch (cell_index->row) {
     // This is the menu item with the cycling icon
+    case 0:
+      //FIX ME: Load Window n_campus and push to top of stack
+      break;
     case 1:
-      // Cycle the icon
-      current_icon = (current_icon + 1) % NUM_MENU_ICONS;
-      // After changing the icon, mark the layer to have it updated
-      layer_mark_dirty(menu_layer_get_layer(menu_layer));
+      //FIX ME: Load Window c_campus and push to top of stack
       break;
   }
-
 }
 
 // This initializes the menu upon window load
 void window_load(Window *window) {
-  // Here we load the bitmap assets
-  // resource_init_current_app must be called before all asset loading
-  int num_menu_icons = 0;
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_BIG_WATCH);
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_SECTOR_WATCH);
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_BINARY_WATCH);
-
-  // And also load the background
-  menu_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND_BRAINS);
 
   // Now we prepare to initialize the menu layer
   // We need the bounds to specify the menu layer's viewport size
@@ -140,10 +73,7 @@ void window_load(Window *window) {
 
   // Set all the callbacks for the menu layer
   menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_sections = menu_get_num_sections_callback,
     .get_num_rows = menu_get_num_rows_callback,
-    .get_header_height = menu_get_header_height_callback,
-    .draw_header = menu_draw_header_callback,
     .draw_row = menu_draw_row_callback,
     .select_click = menu_select_callback,
   });
@@ -158,14 +88,6 @@ void window_load(Window *window) {
 void window_unload(Window *window) {
   // Destroy the menu layer
   menu_layer_destroy(menu_layer);
-
-  // Cleanup the menu icons
-  for (int i = 0; i < NUM_MENU_ICONS; i++) {
-    gbitmap_destroy(menu_icons[i]);
-  }
-
-  // And cleanup the background
-  gbitmap_destroy(menu_background);
 }
 
 ////////////////////////Handles messages
@@ -193,7 +115,7 @@ void window_unload(Window *window) {
   // Act on the found fields received
   if (text_tuple) {
     
-      menu_cell_basic_header_draw(context, header, text_tuple->value->cstring);
+      
       APP_LOG(APP_LOG_LEVEL_DEBUG, "Text: %s", text_tuple->value->cstring);
   }
  }
