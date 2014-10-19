5e14245ce0eb2604801919b6e450bcfea0260636
Ishdeep Baid <ishbaid@umich.edu>
2014-10-19 00:27:26 -0400

    stuff

--- src/feature_menu_layer.c
+++ src/feature_menu_layer.c
@@ -1,65 +1,22 @@
 #include "pebble.h"
 
-#define NUM_FIRST_MENU_ITEMS 2
 
 static Window *window;
-static Window *n_campus;
-static Window *c_campus;
-
+TextLayer *header;
 
 // This is a menu layer
 // You have more control than with a simple menu layer
-static MenuLayer *menu_layer;
-static MenuLayer *n_campus_layer;
-static MenuLayer *c_campus_layer;
+
+static int counter = 0;
 
 static char *title;
 
 // Each section has a number of items;  we use a callback to specify this
 // You can also dynamically add and remove items using this
-static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  switch (section_index) {
-    case 0:
-      return NUM_FIRST_MENU_ITEMS;
 
-    default:
-      return 0;
-  }
-}
 
-// This is the menu item draw callback where you specify what each item should look like
-static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
-  // Determine which section we're going to draw in
-  switch (cell_index->section) {
-    case 0:
-      // Use the row to specify which item we'll draw
-      switch (cell_index->row) {
-        case 0:
-          // This is a basic menu item with a title
-          menu_cell_basic_draw(ctx, cell_layer, title, NULL, NULL);
-          break;
-        
-        case 1:
-          menu_cell_basic_draw(ctx, cell_layer, "Central Campus", NULL, NULL);
-          break;
-      }
-      break;
-  }
-}
 
-// Here we capture when a user selects a menu item
-void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-  // Use the row to specify which item will receive the select action
-  switch (cell_index->row) {
-    // This is the menu item with the cycling icon
-    case 0:
-      //FIX ME: Load Window n_campus and push to top of stack
-      break;
-    case 1:
-      //FIX ME: Load Window c_campus and push to top of stack
-      break;
-  }
-}
+
 
 // This initializes the menu upon window load
 void window_load(Window *window) {
@@ -67,31 +24,21 @@ void window_load(Window *window) {
   // Now we prepare to initialize the menu layer
   // We need the bounds to specify the menu layer's viewport size
   // In this case, it'll be the same as the window's
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_frame(window_layer);
+  //Layer *window_layer = window_get_root_layer(window);
+  //GRect bounds = {0, 0, };
 
-  // Create the menu layer
-  menu_layer = menu_layer_create(bounds);
-
-  // Set all the callbacks for the menu layer
-  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_rows = menu_get_num_rows_callback,
-    .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
-  });
-
-  // Bind the menu layer's click config provider to the window for interactivity
-  menu_layer_set_click_config_onto_window(menu_layer, window);
+  header = text_layer_create(GRect(0, 0 , 144, 60));
 
   // Add it to the window for display
   title = malloc(14);
   title = "Bus Stop Name";
-  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
+
 }
 
 void window_unload(Window *window) {
   // Destroy the menu layer
-  menu_layer_destroy(menu_layer);
+  text_layer_destroy(header);
+
 }
 
 ////////////////////////Handles messages
@@ -104,16 +51,10 @@ void window_unload(Window *window) {
    // outgoing message failed
  }
 
-  
-  enum {
-       AKEY_NUMBER,
-       AKEY_TEXT,
-  };
-
  void in_received_handler(DictionaryIterator *iter, void *context) {
    // incoming message received
   // Check for fields you expect to receive
-  int KEY = 1; 
+  /*int KEY = 1; 
   Tuple *text_tuple = dict_find(iter, KEY);
 
   // Act on the found fields received
@@ -121,18 +62,55 @@ void window_unload(Window *window) {
     
       
       APP_LOG(APP_LOG_LEVEL_DEBUG, "Text: %s", text_tuple->value->cstring);
-      
-      free(title);
+      // if(title){
+
+      //   //free(title);
+      //   title = NULL;
+      // }
       title = malloc(text_tuple->length);
       strncpy(title, text_tuple->value->cstring, text_tuple->length);
-      menu_layer_reload_data(n_campus_layer);
-  }
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Title: %s", title);
+      text_layer_set_text(header, title);
+      layer_add_child(window_get_root_layer(window), text_layer_get_layer(header));
+
+  }*/
+
+
+      Tuple *text_tuple; 
+      switch(counter){
+
+        case(0):
+
+        text_tuple = dict_find(iter, 0);
+        title = malloc(text_tuple->length);
+        strncpy(title, text_tuple->value->cstring, text_tuple->length);
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Title: %s", title);
+        break;
+        case(1):
+
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Not active rn");
+
+        break;
+        case(2):
+
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Not active rn");
+
+        break;
+        default:
+
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Default");
+        break;
+
+      };
+
+      counter ++;
 
  }
 
 
  void in_dropped_handler(AppMessageResult reason, void *context) {
    // incoming message dropped
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "REASON: %i", reason);
  }
 
 int main(void) {
